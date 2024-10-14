fc266447ebac7d5af7fa44c826bf59255a54b530
Dmitry <Dololob92@gmail.com>
2024-10-14 11:11:10 +0300

    test complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let numStr = String(n);
+  let mas = numStr.split('').reverse().join('');
+  let result = mas.replace( '-', '');
+  let num = Number(result);
+  return num;
 }
